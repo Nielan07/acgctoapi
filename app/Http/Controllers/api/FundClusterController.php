16e9dd595d8b405ea4d7d89408f80b62bc5552ad
Nielan <nielan07@gmail.com>
2025-02-11 16:26:19 +0800

    acg updates

---
+++ app/Http/Controllers/api/FundClusterController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FundCluster;
+use Illuminate\Http\Request;
+
+class FundClusterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FundCluster $fundCluster)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FundCluster $fundCluster)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, FundCluster $fundCluster)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FundCluster $fundCluster)
+    {
+        //
+    }
+}
